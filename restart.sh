fba26a2895b1f237df5f1e026f7ceb0be6d3d543
UltimateForm <UltimateForm@github.com>
2025-09-24 15:25:06 +0100

    update start script

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-docker stop dc-lb-lite
-docker rm dc-lb-lite
-docker build . -t dc-lb-lite-img
-docker run -d -v ./persist/:/bot/persist/ --name dc-lb-lite dc-lb-lite-img
+docker stop kov
+docker rm kov
+docker build . -t kov-img
+docker run -d -v ./persist/:/bot/persist/ --name kov kov-img
