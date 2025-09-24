3e0cc40df2c9d01aafc5049102eae67544a8e20f
UltimateForm <UltimateForm@github.com>
2025-09-24 14:38:42 +0100

    fix container name

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-docker stop dc-lb-lite
-docker rm dc-lb-lite
-docker build . -t dc-lb-lite-img
-docker run -d -v ./persist/:/bot/persist/ --name dc-lb-lite dc-lb-lite-img
+docker stop rbb
+docker rm rbb
+docker build . -t rbb-img
+docker run -d -v ./persist/:/bot/persist/ --name rbb rbb-img
